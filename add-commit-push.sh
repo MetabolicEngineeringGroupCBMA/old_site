419cc63f981c8996a591213ffa945d96adc98b12
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2018-01-16 11:11:09 +0000

    no comment

--- add-commit-push.sh
+++ add-commit-push.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git add --all&&git commit -m "no comment"&&git push origin&&echo done!&&read -rpn1
+git add --all&&git commit -m "no comment"&&git push origin&&echo done!&&read -rn1 -p

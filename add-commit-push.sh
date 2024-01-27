2b812ba0c555929afabd7ee97abf9e13c00dbd2d
BjornFJohansson <bjornjobb@gmail.com>
2024-01-27 21:47:31 +0000

    removed files

--- add-commit-push.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git config credential.helper store
-git add --all&&git commit -m "no comment"&&git push origin&&echo done!&&read -rn1
--- runjekyllandserve.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash 
-/home/bjorn/gems/bin/jekyll serve
